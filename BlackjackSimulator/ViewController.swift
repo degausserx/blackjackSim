f87240f541699b4302c87780a8ffd4ca0fffb7d2
etudiant <etudiant@etudiants-Mac.local>
2017-10-02 10:36:41 +0100

    Initial Commit

---
+++ BlackjackSimulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlackjackSimulator
+//
+//  Created by etudiant on 02/10/2017.
+//  Copyright © 2017 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
